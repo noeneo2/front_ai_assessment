02ff05cfa1c110f3ddab551363f6f07f3e7e4225
Noe <noeli.pantigoso@neo.com.pe>
2025-10-31 15:49:34 -0500

    commit

---
+++ src/views/subiendo.js
@@ -0,0 +1,238 @@
+import React from 'react'
+
+import { Helmet } from 'react-helmet'
+
+import './page.css'
+
+const Page = (props) => {
+  return (
+    <div className="page-container1">
+      <Helmet>
+        <title>Blond Relieved Barracuda</title>
+        <meta property="og:title" content="Blond Relieved Barracuda" />
+      </Helmet>
+      <div className="page-subiendo">
+        <div className="page-content">
+          <div className="page-header">
+            <div className="page-div">
+              <div className="page-logonegro-neo11">
+                <div className="page-clippathgroup1">
+                  <div className="page-clip05707104601">
+                    <img
+                      src="/external/vectori281-o9q4n.svg"
+                      alt="VectorI281"
+                      className="page-vector10"
+                    />
+                  </div>
+                  <div className="page-group1">
+                    <img
+                      src="/external/vectori281-d2v4.svg"
+                      alt="VectorI281"
+                      className="page-vector11"
+                    />
+                    <img
+                      src="/external/vectori281-4n5m.svg"
+                      alt="VectorI281"
+                      className="page-vector12"
+                    />
+                    <img
+                      src="/external/vectori281-mam9.svg"
+                      alt="VectorI281"
+                      className="page-vector13"
+                    />
+                    <img
+                      src="/external/vectori281-gglr.svg"
+                      alt="VectorI281"
+                      className="page-vector14"
+                    />
+                    <img
+                      src="/external/vectori281-wpyp.svg"
+                      alt="VectorI281"
+                      className="page-vector15"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="page-main-content">
+            <div className="page-upload-module">
+              <div className="page-title">
+                <span className="page-text1">
+                  Transforma tu excel en un dashboard de Madurez Organizacional
+                  en Gen AI
+                </span>
+              </div>
+              <div className="page-upload">
+                <div className="page-frame8">
+                  <div className="page-frame7">
+                    <div className="page-frame6">
+                      <div className="page-frame5">
+                        <div className="page-group4">
+                          <div className="page-group6">
+                            <div className="page-group5">
+                              <img
+                                src="/external/rectangle72810-hb6.svg"
+                                alt="Rectangle72810"
+                                className="page-rectangle7"
+                              />
+                              <img
+                                src="/external/rectangle82810-k6rr-200h.png"
+                                alt="Rectangle82810"
+                                className="page-rectangle8"
+                              />
+                            </div>
+                          </div>
+                        </div>
+                        <div className="page-frame4">
+                          <div className="page-frame1321316828">
+                            <span className="page-text2">base.xlsx</span>
+                            <div className="page-group11">
+                              <div className="page-vuesaxoutlineclosecircle1">
+                                <div className="page-vuesaxoutlineclosecircle2">
+                                  <div className="page-closecircle">
+                                    <img
+                                      src="/external/vector2810-75mq.svg"
+                                      alt="Vector2810"
+                                      className="page-vector16"
+                                    />
+                                    <img
+                                      src="/external/vector2810-p85o.svg"
+                                      alt="Vector2810"
+                                      className="page-vector17"
+                                    />
+                                    <img
+                                      src="/external/vector2810-3zbjq.svg"
+                                      alt="Vector2810"
+                                      className="page-vector18"
+                                    />
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                          <div className="page-frame3">
+                            <span className="page-text3">60 KB de 12O KB</span>
+                            <div className="page-frame2">
+                              <div className="page-group7">
+                                <img
+                                  src="/external/loading2810-7gjl.svg"
+                                  alt="Loading2810"
+                                  className="page-loading"
+                                />
+                              </div>
+                              <span className="page-text4">Subiendo...</span>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="page-group14">
+                      <img
+                        src="/external/vector22810-hevg.svg"
+                        alt="Vector22810"
+                        className="page-vector2"
+                      />
+                      <img
+                        src="/external/vector32810-3fbd.svg"
+                        alt="Vector32810"
+                        className="page-vector3"
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className="page-frame15">
+                  <span className="page-text5">
+                    Continuar
+                    <span
+                      dangerouslySetInnerHTML={{
+                        __html: ' ',
+                      }}
+                    />
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="page-footer">
+            <div className="page-page-links">
+              <div className="page-logos">
+                <div className="page-logo">
+                  <div className="page-logonegro-neo12">
+                    <div className="page-clippathgroup2">
+                      <div className="page-clip05707104602">
+                        <img
+                          src="/external/vectori281-vqcb.svg"
+                          alt="VectorI281"
+                          className="page-vector19"
+                        />
+                      </div>
+                      <div className="page-group2">
+                        <img
+                          src="/external/vectori281-45wj.svg"
+                          alt="VectorI281"
+                          className="page-vector20"
+                        />
+                        <img
+                          src="/external/vectori281-ojzf.svg"
+                          alt="VectorI281"
+                          className="page-vector21"
+                        />
+                        <img
+                          src="/external/vectori281-hpiw.svg"
+                          alt="VectorI281"
+                          className="page-vector22"
+                        />
+                        <img
+                          src="/external/vectori281-tx6g.svg"
+                          alt="VectorI281"
+                          className="page-vector23"
+                        />
+                        <img
+                          src="/external/vectori281-edma.svg"
+                          alt="VectorI281"
+                          className="page-vector24"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="page-footer-link-column">
+                <span className="page-text6">Neo Consulting. 2025</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <a href="https://play.teleporthq.io/signup" className="page-link">
+        <div aria-label="Sign up to TeleportHQ" className="page-container2">
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 19 21"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className="page-icon1"
+          >
+            <path
+              d="M9.1017 4.64355H2.17867C0.711684 4.64355 -0.477539 5.79975 -0.477539 7.22599V13.9567C-0.477539 15.3829 0.711684 16.5391 2.17867 16.5391H9.1017C10.5687 16.5391 11.7579 15.3829 11.7579 13.9567V7.22599C11.7579 5.79975 10.5687 4.64355 9.1017 4.64355Z"
+              fill="#B23ADE"
+            ></path>
+            <path
+              d="M10.9733 12.7878C14.4208 12.7878 17.2156 10.0706 17.2156 6.71886C17.2156 3.3671 14.4208 0.649963 10.9733 0.649963C7.52573 0.649963 4.73096 3.3671 4.73096 6.71886C4.73096 10.0706 7.52573 12.7878 10.9733 12.7878Z"
+              fill="#FF5C5C"
+            ></path>
+            <path
+              d="M17.7373 13.3654C19.1497 14.1588 19.1497 15.4634 17.7373 16.2493L10.0865 20.5387C8.67402 21.332 7.51855 20.6836 7.51855 19.0968V10.5141C7.51855 8.92916 8.67402 8.2807 10.0865 9.07221L17.7373 13.3654Z"
+              fill="#2874DE"
+            ></path>
+          </svg>
+          <span className="page-text7">Built in TeleportHQ</span>
+        </div>
+      </a>
+    </div>
+  )
+}
+
+export default Page
