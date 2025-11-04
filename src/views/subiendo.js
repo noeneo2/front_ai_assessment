cc47807afd7aad8590a5c305af6c61628fdb9103
noelia.pantigoso <noelia.pantigoso@neo.com.pe>
2025-11-04 20:12:26 +0000

    inicio

--- src/views/subiendo.js
+++ src/views/subiendo.js
@@ -8,45 +8,60 @@ import './subiendo.css';
 const Subiendo = (props) => {
   const { fileName } = useContext(ReportContext);
 
-  return (
-    <div className="page-container1">
-      <Helmet>
-        <title>Archivo Cargado - Madurez Organizacional Gen AI</title>
-      </Helmet>
+    return (
+      <div className="page-container1">
+        <Helmet>
+          <title>Archivo Cargado - Madurez Organizacional Gen AI</title>
+        </Helmet>
+
       <div className="page-subiendo">
-        <div className="page-content">
-          <div className="page-header"></div>
+          <div className="page-content">
+            <header className="page-header">
+              <img
+                className="page-header-logo"
+                src="/external/Logo_negro_NEO_header.svg"
+                alt="Logo Neo"
+              />
+                                                                                                                                </header>
 
-          <div className="page-main-content">
-            <div className="page-upload-module">
-              <div className="page-title">
-                <span className="page-text1">
-                  Archivo cargado exitosamente
-                </span>
-              </div>
-              <div className="page-upload">
-                <div className="page-frame8">
-                  <div className="page-frame4" style={{ alignItems: 'center', width: '100%' }}>
-                    <div className="page-frame1321316828">
-                      <span className="page-text2">{fileName || 'No se ha seleccionado archivo'}</span>
-                    </div>
-                  </div>
-                </div>
-                
-                <Link to="/cargando" className="page-frame15" style={{ textDecoration: 'none' }}>
-                  <span className="page-text5">
-                    Continuar
-                  </span>
-                </Link>
-              </div>
-            </div>
-          </div>
 
-          <div className="page-footer"></div>
-        </div>
-      </div>
-    </div>
-  );
-}
+                                                                                                                                          <main className="page-main-content">
+                                                                                                                                                      <div className="page-upload-module">
+                                                                                                                                                                    <div className="page-title">
+                                                                                                                                                                                    <span className="page-text1">
+                                                                                                                                                                                                      Archivo cargado exitosamente
+                                                                                                                                                                                                                      </span>
+                                                                                                                                                                                                                                    </div>
+                                                                                                                                                                                                                                                  <div className="page-upload">
+                                                                                                                                                                                                                                                                  <div className="page-frame8">
+                                                                                                                                                                                                                                                                                    <div className="page-frame4" style={{ alignItems: 'center', width: '100%' }}>
+                                                                                                                                                                                                                                                                                                        <div className="page-frame1321316828">
+                                                                                                                                                                                                                                                                                                                              <span className="page-text2">{fileName || 'No se ha seleccionado archivo'}</span>
+                                                                                                                                                                                                                                                                                                                                                  </div>
+                                                                                                                                                                                                                                                                                                                                                                    </div>
+                                                                                                                                                                                                                                                                                                                                                                                    </div>
+                                                                                                                                                                                                                                                                                                                                                                                                    
+                                                                                                                                                                                                                                                                                                                                                                                                                    <Link to="/cargando" className="page-frame15" style={{ textDecoration: 'none' }}>
+                                                                                                                                                                                                                                                                                                                                                                                                                                      <span className="page-text5">
+                                                                                                                                                                                                                                                                                                                                                                                                                                                          Continuar
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                            </span>
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            </Link>
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          </div>
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      </div>
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                </main>
 
-export default Subiendo;
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          <footer className="page-footer">
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      <img
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  className="page-footer-logo"
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              src="/external/Logo_negro_NEO_footer.svg"
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          alt="Logo Neo"
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      />
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                </footer>
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        </div>
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              </div>
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  </div>
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    );
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    }
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    export default Subiendo;
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
