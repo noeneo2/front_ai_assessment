0774a700687dbb50aef0a5d18562daf3e230b5e7
noelia.pantigoso <noelia.pantigoso@neo.com.pe>
2025-11-20 14:24:16 +0000

    f

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAkUgybj5u87z8anqjBv8vyxuZ-NC1nVrs",
-  authDomain: "pruebas-genai.firebaseapp.com",
-  databaseURL: "https://pruebas-genai-default-rtdb.firebaseio.com",
-  projectId: "pruebas-genai",
-  storageBucket: "pruebas-genai.appspot.com",
-  messagingSenderId: "125734547785",
-  appId: "1:125734547785:web:fd68b282d81321eb136a98"
+  
 };
 
 const app = initializeApp(firebaseConfig);
