11471eb6aa32260fdbdccb6b10722ada9a3dfc98
Noe <noeli.pantigoso@neo.com.pe>
2025-11-21 12:30:35 -0500

    abc

--- src/views/projectpage.js
+++ src/views/projectpage.js
@@ -131,10 +131,12 @@ const ProjectPage = (props) => {
       </Helmet>
       <div className="projectpage-header">
         <img alt="Logo" src="/external/Logo_negro_NEO_header.svg" className="projectpage-header-logo" />
-        <div className="projectpage-header-button" onClick={handleLogout}>
-          <img alt="Logout Icon" src="/external/iconlogoutea4f.svg" className="projectpage-logout-icon" />
-          <span className="projectpage-button-text">Logout</span>
-        </div>
+        <button className="projectpage-nav-button" onClick={handleLogout}>
+          <svg className="projectpage-nav-button-icon" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M17 7l-1.41 1.41L18.17 11H8v2h10.17l-2.58 2.58L17 17l5-5zM4 5h8V3H4c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h8v-2H4V5z" fill="currentColor" />
+          </svg>
+          <span>Logout</span>
+        </button>
       </div>
       <div className="projectpage-main-content">
         <div className="projectpage-title-container">
